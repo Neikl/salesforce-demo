ae03c9478e12e1ba5629977cf7946d127b0be926
Neikl <neiklohit62@gmail.com>
2020-09-15 21:43:32 +0530

    initial commit

---
+++ sample/post-deploy.sh
@@ -0,0 +1,4 @@
+sfdx force:user:permset:assign -n purealoe -u test-xd71223tooya@example.com
+
+#sfdx force:data:tree:import --plan ./data/Harvest_Field__c-plan.json -u test-mjqc6ffwlczm@example.com
+#sfdx force:data:tree:import --plan ./data/Merchandise__c-plan.json -u test-mjqc6ffwlczm@example.com
